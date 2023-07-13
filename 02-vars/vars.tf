4f11262c24ece6b3766d1efe4e6c38245cb7d3ad
sneha <snehasreeramini@gmail.com>
2023-07-13 15:43:23 +0200

    variables in terraform

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -5,3 +5,51 @@ variable "example"{
 output "example"{
   value = var.example
 }
+
+#######data types
+#String data types
+variable "sample1"{
+  default = "Hello World"
+}
+
+#Numeber data types
+variable "sample2"{
+  default = 100
+}
+
+#Boolean data types
+variable "sample3"{
+  default = true
+}
+
+
+##variable typrs
+variable "sample4"{
+  type = "list" #optional
+  default = [
+  "Hello",
+    1000,
+    true,
+    "world"
+  ]
+}
+
+variable "sample5"{
+  type = "map"
+  default = {
+    string  = "Hello",
+    number  = 1000,
+    boolean = true
+  }
+
+}
+
+
+output "sample4"{
+  value = var.sample4[0]
+}
+
+
+output "sample5"{
+  value = var.sample5["number"]
+}
