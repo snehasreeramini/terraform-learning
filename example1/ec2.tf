4e4fdeb5c1f9c1326b002d674927a7405b0730f1
sneha <snehasreeramini@gmail.com>
2023-07-13 17:09:05 +0200

    create ec2 instance with t3.micro with security group

--- example1/ec2.tf
+++ example1/ec2.tf
@@ -5,8 +5,30 @@ provider "aws"{
 resource "aws_instance" "sample" {
   ami                   =  "ami-0bb6af715826253bf"
   instance_type         =  "t3.micro"
+  vpc_security_group_ids = [aws_default_security_group.default.id]
 }
 
-output "public_ip"{
+output "public_ip" {
   value = aws_instance.sample.public_ip
+}
+resource "aws_default_security_group" "default" {
+   name             =     "allow-tls"
+   description      =     "allow TLS inbound traffic"
+
+  ingress {
+    description =  "TLS FROM VPC"
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 }
